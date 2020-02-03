4ac1c251c2664fae5d59d24935f45e907fdcaf20
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-02-03 18:20:23 -0500

    change scrape function from its own js to server.js and try to see if it works for heroku

--- server.js
+++ server.js
@@ -49,10 +49,58 @@ mongoose.connect(MONGODB_URI, {
 
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    scrapeIt()
-    res.send("scraped")
-});
+    // scrapeIt()
+    // Make a request via axios to grab the HTML body from the site of your choice
+    axios.get("https://www.thetimes.co.uk").then(function (response) {
 
+        // Load the HTML into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        let $ = cheerio.load(response.data);
+
+        // An empty array to save the data that we'll scrape
+        // let results = [];
+        // Select each element in the HTML body from which you want information.
+        // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+        // but be sure to visit the package's npm page to see how it works
+        if ($(".Item-content") !== "") {
+            let result = {};
+
+            $(".Item-content").each(function (i, element) {
+                // let title = $(element).children("h3").text();
+                // let summary = $(element).children("p").text();
+                // let link = $(element).find("a").attr("href");
+                if ($(element).children("p").text() !== '') {
+                    // results.push({
+                    //     title: title,
+                    //     summary: summary,
+                    //     link: link
+                    // })
+                    result.title = $(this).children("h3").text();
+                    result.summary = $(this).children("p").text();
+                    result.link = $(this).find("a").attr("href");
+                } else {
+                    // results.push({
+                    //     title: title,
+                    //     link: link
+                    // })
+                    result.title = $(this).children("h3").text();
+                    result.link = $(this).find("a").attr("href");
+                }
+                // Create a new Article using the `result` object built from scraping
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        // View the added result in the console
+                        // console.log(dbArticle);
+                    })
+                    .catch(function (err) {
+                        // If an error occurred, log it
+                        console.log(err);
+                    });
+            })
+        }
+        res.send("scraped")
+    });
+})
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
     // Grab every document in the Articles collection
